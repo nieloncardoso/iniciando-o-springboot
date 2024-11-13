f3dc6b9690c1fe2dc4628bd99b4e2acded17a28c
nieloncardoso <nielon.reis@youxlab.com.br>
2024-11-13 17:58:43 -0300

    inicio

---
+++ primeiro_projeto_spring/src/test/java/com/youxlab/primeiro_projeto_spring/PrimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youxlab.primeiro_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
